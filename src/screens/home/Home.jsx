96fee78f6382dd023b586da19884a1b6af156c46
Astral Developers <ehtisham57>
2024-05-10 11:32:34 +0500

    abc

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import HomeSec2 from "../../components/HomeSec2";
-
+import Portfolio from "@/components/Portfolio";
+import HomeSec3 from "@/components/HomeSec3";
+import Footer from "@/components/Footer/Footer";
 
 const Home = () => {
+  const text = <h1>Explore more multimedia life</h1>
+  const peragraph = <p>Don't stop—there's a whole ocean full of amazing creatures waiting to be discovered! From the colorful coral reefs to the mysterious depths, each layer has its own set of residents.</p>
 
   return (
     <>
     
-     <HomeSec2/>
-
-      
+     <Portfolio/>
+     <HomeSec3  heading={text} pera={peragraph}/>
+     <Footer/>
     </>
   );
 };
